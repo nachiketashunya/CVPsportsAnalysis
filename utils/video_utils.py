f3625333c17f085b438cec1b010f1069374db2eb
Nachiketa Purohit <54649938+nachiketashunya@users.noreply.github.com>
2024-04-28 22:25:12 +0530

    Update video_utils.py

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -11,6 +11,7 @@ def read_video(video_path):
     return frames
 
 def save_video(output_video_frames,output_video_path):
+    os.makedirs("output", exist_ok=True)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_video_path,fourcc,24,(output_video_frames[0].shape[1],output_video_frames[0].shape[0]))
     for frame in output_video_frames:
